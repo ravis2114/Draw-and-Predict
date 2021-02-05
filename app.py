291e17ca4c26885280782613aebbe15e66843254
Ravi Shankar <ravis2114@gmail.com>
2021-02-05 21:24:20 +0530

    ajax url changed

--- app.py
+++ app.py
@@ -32,22 +32,22 @@ def hello_image():
 
     ######### converting string data into bytes
     imgdata = base64.b64decode(image_data)
-    print(len(imgdata))
+    #print(len(imgdata))
 
-    print ('Image received and ready to be processed')
+    #print ('Image received and ready to be processed')
 
     
     #########-----DATA PREPROCESSING, FROM string to bytes to array -----##############
     # NOTES-----a string is a sequence of characters, ie unicode codepoints; these are an abstract concept, and can't be directly stored on disk. A byte string is a sequence of, unsurprisingly, bytes - things that can be stored on disk. The mapping between them is an encoding 
     img=np.frombuffer(imgdata, np.uint8) #converting byte data to 1-D numpy array
-    print(img.shape)
+    #print(img.shape)
     img=cv2.imdecode(img, cv2.IMREAD_COLOR) #converting 1-D array to original canvas data of shape 500x500x3
-    print(img.shape) #same as shape of canvas i.e, 500x500 x3(see html file)
+    #print(img.shape) #same as shape of canvas i.e, 500x500 x3(see html file)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting image to GRAY, i.e, shape becomes 500x500
-    print(img.shape)
+    #print(img.shape)
     img = cv2.resize(img, dsize=(28, 28), interpolation=cv2.INTER_AREA) # resizing to required 28x28, INTER_AREA IS USED WHEN SHRINKING AND OTHER INTERPOLATION METHOD SUCH AS INTER_CUBIC INTER_LINEAR etc WHILE ENLARGING 
     img = img/255
-    print(img.shape)
+    #print(img.shape)
    
 
     ###############----PREDICTION----###########################
@@ -64,4 +64,4 @@ def hello_image():
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) #127.0.0.1 is the loopback address (also known as localhost). 0.0.0.0 is a non-routable meta-address used to designate an invalid, unknown or non applicable target (a no particular address placeholder). use 127.0.0.1 when run locally
--- static/jscode.js
+++ static/jscode.js
@@ -57,7 +57,7 @@ function SaveImg() {
     
     $.ajax({
           type: "POST",
-          url: "http://127.0.0.1:5000/imgrec",
+          url: "{{url_for('hello_image')}}",
           data:{
             imageBase64: dataURL
           }
