6bf8ba36aeef65f34dfb2acca062785bade2bb54
Ravi Shankar <ravis2114@gmail.com>
2021-06-10 14:23:03 +0530

    css fixes

--- train_mod.py
+++ train_mod.py
@@ -1,70 +1,25 @@
+import tensorflow as tf
+from PIL import Image
 import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
 
+mnist = tf.keras.datasets.mnist
 
-#######################----DATA PREPARING----#################
-# Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
+model.fit(x_train, y_train, epochs=100)
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+model.evaluate(x_test,  y_test, verbose=2)
 
-
-
-#############################-----MODEL------------###################
-model = keras.Sequential(
-    [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
-
-model.summary()
-
-#############################------TRAIN--------#################
-batch_size = 128
-epochs = 15
-
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-
-#################-----EVALUATION-------------###############
-score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
-
-#################----SAVE MODEL------###################
-model.save("my_model.h5")
-
-
-'''
-
-
-model=keras.models.load_model("my_model.h5")
-pred= model.predict(x_test[10].reshape(1,28,28,1)) #input should be 4-D (batch_size, dimensions)
-print(np.argmax(pred), np.argmax(y_test[10]))
-'''
+model.save('my_model.h5')
